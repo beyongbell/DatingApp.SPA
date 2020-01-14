364cde26ea3494a34884770cd3dd63d29607b261
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-01-14 07:36:37 +0700

    Add Pagination

--- src/app/resolvers/member-list.resolver.ts
+++ src/app/resolvers/member-list.resolver.ts
@@ -8,9 +8,12 @@ import { User } from '@interface/user';
 
 @Injectable()
 export class MemberListResolver implements Resolve<User> {
+  pageNumber = 1;
+  pageSize   = 5;
+
   constructor(private userService: UserService, private router: Router, private alertifyService: AlertifyService) {}
   resolve(route: ActivatedRouteSnapshot): Observable<User> {
-    return this.userService.getUsers().pipe(
+    return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
       catchError(error => {
         this.alertifyService.error('Problem retrieving data');
         this.router.navigate(['/home']);
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,10 @@
+import { PaginatedResult } from './../interfaces/pagination';
 import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { User } from '@interface/user';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +15,24 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.baseUrl + 'users');
+  getUsers(page?, itemsPerPage?): Observable<PaginatedResult<User[]>> {
+
+    const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
+
+    let params = new HttpParams();
+
+    if (page != null && itemsPerPage != null) {
+      params = params.append('pageNumber', page);
+      params = params.append('pageSize', itemsPerPage);
+    }
+
+    return this.http.get<User[]>(this.baseUrl + 'users', { observe: 'response', params}).pipe(map(response => {
+      paginatedResult.result = response.body;
+      if (response.headers.get('Pagination') != null) {
+        paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
+      }
+      return paginatedResult;
+    }));
   }
 
   getUser(id): Observable<User> {
